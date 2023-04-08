be71d2e6630ddba361ff87d2d3de3fc22523c2af
Kunal <kunal20vijay00@gmail.com>
2023-04-08 13:45:32 +0530

    first commit

---
+++ src/component/Main.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the world of Tweets</h1>
+        <h6>Tweets Sentiment Analysis</h6>
+        <div>
+          <div>
+            <h3>350632</h3>
+            <p>Tweets sent per minute</p>
+          </div>
+          <div>
+            <h3>321056231</h3>
+            <p>Total Number of monthly active users on Twitter</p>
+          </div>
+          <div>
+            <h3>7447680</h3>
+            <p>Minutes Since First Tweet Ever</p>
+          </div>
+        </div>
+      </div>
+      <h2>Perform Sentiment Analysis on Tweets</h2>
+      <div>
+        {/* tweeter image  */}
+      </div>
+    </>
+  );
+}
